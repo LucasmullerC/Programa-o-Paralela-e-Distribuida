29743b8472a1c433a66d1e4ee0b175e4cca78082
Lucas Muller <lucas.mullerqdv@gmail.com>
2022-08-28 19:20:09 -0300

    ATV 3

--- ATV3/TCPServidor.java
+++ ATV3/TCPServidor.java
@@ -1,3 +1,24 @@
-public class TCPServidor {
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Date;
 
+public class TCPServidor {
+    public static void main(String[] args) {
+        try {
+            ServerSocket servidor = new ServerSocket(8089);
+            System.out.println("Servidor ouvindo a porta 8089");
+            while (true) {
+                Socket cliente = servidor.accept();
+                System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
+                ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+                saida.flush();
+                saida.writeObject(new Date(0));
+                saida.close();
+                cliente.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
 }
